3641a4c2c57a3415b2a9ca81217e3ede4b757f43
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-14 15:04:45 +0200

    fixed the error with pundit and the new models

---
+++ app/policies/conversation_policy.rb
@@ -0,0 +1,15 @@
+class ConversationPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
