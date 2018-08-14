5d04545a3d9da9974e67159e0b68dc9b2a7636a4
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-14 14:56:11 +0200

    implement edit and delete functions on the show pages of instruments only for the corresponding users

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,14 +1,12 @@
 class InstrumentsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :find_instrument, only: [:show, :edit, :update, :destroy]
   
   def index
     @instruments = policy_scope(Instrument)
   end
 
-  def show
-    @instrument = Instrument.find(params[:id])
-    authorize @instrument
-  end
+  def show; end
 
   def new
     @instrument = Instrument.new
@@ -26,7 +24,27 @@ class InstrumentsController < ApplicationController
     end
   end
 
+  def edit;end
+
+  def update
+    if @instrument.update(instrument_params)
+      redirect_to instrument_path(@instrument)
+    else
+      render :update
+    end
+  end
+
+  def destroy
+    @instrument.destroy
+    redirect_to instruments_path
+  end
+
   private
+
+  def find_instrument
+    @instrument = Instrument.find(params[:id])
+    authorize @instrument
+  end
 
   def instrument_params
     params.require(:instrument).permit(:name, :description, :photo, :location, :price)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'instruments#index'
-  resources :instruments, only: [:index, :show, :new, :create]
+  resources :instruments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
