79e2b9c766c1b3c19efb3444728d5612818d12e4
iain russell <iainrussell89@gmail.com>
2018-08-14 17:41:26 +0200

    Working on booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,32 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = policy_scope(Booking)
+    # @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
+  end
+
+  def show 
+    @booking = Booking.find(params[:id])
+    @user = User.find(@booking.user_id)
+    authorize @booking
+    authorize @user
+  end
+
+
+  def create
+    @booking = Booking.new(booking_params)
+    authorize @booking
+    if @booking.save
+      redirect_to booking_path(@booking)
+      authorize @booking
+    else
+      render :new
+      authorize @booking
+    end
+  end
+
+   private
+    def booking_params
+      params.permit(:user_id, :instrument_id)
+    end
+end
