fc833efdfd16c2f63dc0ff1f2ec1151d4b0d1943
Emily Rhodes <emilyerhodes@hotmail.com>
2018-08-16 16:53:28 +0200

    seeeeeeeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,52 @@ user3 = User.create(email: "test3@example.com", password: "123456789", name: Fak
 user4 = User.create(email: "test4@example.com", password: "123456789", name: Faker::FunnyName.name)
 users = User.all
 
-urls = ['https://images.pexels.com/photos/42224/accord-acoustic-art-background-42224.jpeg?cs=srgb&dl=acoustic-guitar-classical-guitar-concert-guitar-42224.jpg&fm=jpg', 'https://images.pexels.com/photos/45243/saxophone-music-gold-gloss-45243.jpeg?cs=srgb&dl=brass-classic-classical-music-45243.jpg&fm=jpg', 'https://images.pexels.com/photos/1021142/pexels-photo-1021142.jpeg?cs=srgb&dl=antique-bedroom-chord-1021142.jpg&fm=jpg', 'https://images.pexels.com/photos/51937/oboe-music-tool-art-51937.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940, https://images.pexels.com/photos/417456/pexels-photo-417456.jpeg?cs=srgb&dl=adult-band-bokeh-417456.jpg&fm=jpg', 'https://images.pexels.com/photos/237464/pexels-photo-237464.jpeg?cs=srgb&dl=drum-drums-musical-instrument-237464.jpg&fm=jpg', 'https://images.pexels.com/photos/165973/pexels-photo-165973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']
-
-12.times do
-  instrument = Instrument.new(name: Faker::Music.instrument, price: Faker::Number.between(10, 100), location: Faker::Address.city, description: Faker::RickAndMorty.quote, user: User.all.sample)
-  instrument.remote_photo_url = urls.sample
-  instrument.save!
-end
-
-
+  piano = Instrument.new(name: "Steinway piano", price: 10, location: "Berlin", description: " Steinway Concert Grand Piano - Model D", user: User.all.sample)
+  piano.remote_photo_url = "https://i.ebayimg.com/images/g/v3kAAOSwzRFaZn9W/s-l1600.jpg"
+  piano.save!
+  flute = Instrument.new(name: "2017 Yamaha flute", price: 30, location: "Portland", description: "YFL-211S flute", user: User.all.sample)
+  flute.remote_photo_url = "https://i.ebayimg.com/images/g/9z4AAOSwImpXpTMn/s-l1600.jpg"
+  flute.save!
+  guitar = Instrument.new(name: "Fender Telecaster guitar", price: 10, location: "Berlin", description: "Fender Special Edition HH Maple Fingerboard Standard Telecaster Sea Foam Pearl", user: User.all.sample)
+  guitar.remote_photo_url = "https://i.ebayimg.com/images/g/alcAAOSwLMJbaHjT/s-l1600.jpg"
+  guitar.save!
+  sax = Instrument.new(name: "Saxophone", price: 15, location: "Columbus", description: "Brand new professional Gold Tenor Saxophone", user: User.all.sample)
+  sax.remote_photo_url = "https://i.ebayimg.com/images/g/HOUAAOSw4LdZ4lhr/s-l1600.jpg"
+  sax.save!
+  drums = Instrument.new(name: "Vintage sonor drum kit", price: 30, location: "Seattle", description: "Sonor Vintage Series 3pc Shell Kit in Vintage Pearl", user: User.all.sample)
+  drums.remote_photo_url = "https://i.ebayimg.com/images/g/kioAAOSwmMZalOI5/s-l1600.jpg"
+  drums.save!
+  piano = Instrument.new(name: "Steinway piano", price: 10, location: "Berlin", description: " Steinway Concert Grand Piano - Model D", user: User.all.sample)
+  piano.remote_photo_url = "https://i.ebayimg.com/images/g/v3kAAOSwzRFaZn9W/s-l1600.jpg"
+  piano.save!
+  flute = Instrument.new(name: "2017 Yamaha flute", price: 30, location: "Portland", description: "YFL-211S flute", user: User.all.sample)
+  flute.remote_photo_url = "https://i.ebayimg.com/images/g/9z4AAOSwImpXpTMn/s-l1600.jpg"
+  flute.save!
+  guitar = Instrument.new(name: "Fender Telecaster guitar", price: 10, location: "Berlin", description: "Fender Special Edition HH Maple Fingerboard Standard Telecaster Sea Foam Pearl", user: User.all.sample)
+  guitar.remote_photo_url = "https://i.ebayimg.com/images/g/alcAAOSwLMJbaHjT/s-l1600.jpg"
+  guitar.save!
+  sax = Instrument.new(name: "Saxophone", price: 15, location: "Columbus", description: "Brand new professional Gold Tenor Saxophone", user: User.all.sample)
+  sax.remote_photo_url = "https://i.ebayimg.com/images/g/HOUAAOSw4LdZ4lhr/s-l1600.jpg"
+  sax.save!
+  drums = Instrument.new(name: "Vintage sonor drum kit", price: 30, location: "Seattle", description: "Sonor Vintage Series 3pc Shell Kit in Vintage Pearl", user: User.all.sample)
+  drums.remote_photo_url = "https://i.ebayimg.com/images/g/kioAAOSwmMZalOI5/s-l1600.jpg"
+  drums.save!
+  piano = Instrument.new(name: "Steinway piano", price: 10, location: "Berlin", description: " Steinway Concert Grand Piano - Model D", user: User.all.sample)
+  piano.remote_photo_url = "https://i.ebayimg.com/images/g/v3kAAOSwzRFaZn9W/s-l1600.jpg"
+  piano.save!
+  flute = Instrument.new(name: "2017 Yamaha flute", price: 30, location: "Portland", description: "YFL-211S flute", user: User.all.sample)
+  flute.remote_photo_url = "https://i.ebayimg.com/images/g/9z4AAOSwImpXpTMn/s-l1600.jpg"
+  flute.save!
+  guitar = Instrument.new(name: "Fender Telecaster guitar", price: 10, location: "Berlin", description: "Fender Special Edition HH Maple Fingerboard Standard Telecaster Sea Foam Pearl", user: User.all.sample)
+  guitar.remote_photo_url = "https://i.ebayimg.com/images/g/alcAAOSwLMJbaHjT/s-l1600.jpg"
+  guitar.save!
+  sax = Instrument.new(name: "Saxophone", price: 15, location: "Columbus", description: "Brand new professional Gold Tenor Saxophone", user: User.all.sample)
+  sax.remote_photo_url = "https://i.ebayimg.com/images/g/HOUAAOSw4LdZ4lhr/s-l1600.jpg"
+  sax.save!
+  drums = Instrument.new(name: "Vintage sonor drum kit", price: 30, location: "Seattle", description: "Sonor Vintage Series 3pc Shell Kit in Vintage Pearl", user: User.all.sample)
+  drums.remote_photo_url = "https://i.ebayimg.com/images/g/kioAAOSwmMZalOI5/s-l1600.jpg"
+  drums.save!
+  sax = Instrument.new(name: "Saxophone", price: 15, location: "Columbus", description: "Brand new professional Gold Tenor Saxophone", user: User.all.sample)
+  sax.remote_photo_url = "https://i.ebayimg.com/images/g/HOUAAOSw4LdZ4lhr/s-l1600.jpg"
+  sax.save!
 
