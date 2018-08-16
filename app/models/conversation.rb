ae98219120f5c821233d00c1fbad30d1020cff8e
Alexander Dubovoy <ajdubovoy@gmail.com>
2018-08-16 15:02:43 +0200

    fixed messaging database issues hopefully

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,6 +2,7 @@ class Conversation < ApplicationRecord
     belongs_to :sender, class_name: "User", foreign_key: "sender_id"
     belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
     has_many :messages, dependent: :destroy
+    belongs_to :booking
   
     validates_uniqueness_of :sender_id, scope: :receiver_id
   
